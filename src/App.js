5ef0dd2c78c89eaefd928a8c42a69e546efd942c
rinda <rindanurh@gmail.com>
2019-05-12 10:28:02 +0700

    Add travis ci

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Route, Switch, Link} from 'react-router-dom';
 import MovieDetail from './MovieDetail'
